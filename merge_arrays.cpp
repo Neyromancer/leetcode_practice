2ce9bf1221af6c23fea4d4b74c810a4f85250f11
Neyromancer <dmitry.kormulev@yandex.ru>
2020-12-19 23:38:12 +0300

    Merge arrays

---
+++ merge_arrays.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+
+int merge( std::vector<int> &nums1, int m,
+           std::vector<int> &nums2, int n ) {
+  int index = m + n - 1;
+  int i = m - 1;
+  int j = n - 1;
+  while( j >= 0 ) {
+    if( i >= 0 && nums1[ i ] <= nums2[ j ] ) || ( i < 0 ) {
+      nums1[ index-- ] = nums2[ j-- ];
+    } else {
+      nums1[ index-- ] = nums1[ i-- ];
+    }
+  }
+}
