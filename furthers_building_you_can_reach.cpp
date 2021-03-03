3e664dee6d7df8ad6d6e772c73acb76cd9c672d2
Neyroman <neyroman@neyroman.net>
2021-03-03 23:16:47 +0300

    Time Based Key Value Store

--- furthers_building_you_can_reach.cpp
+++ furthers_building_you_can_reach.cpp
@@ -49,3 +49,37 @@ bool can_move_further( std::vector<int> &&heights, int bricks, int ladders )
 
   return true;
 }
+
+// ==================================================
+// space O( N )
+// time O( N log N )
+
+
+#include <vector>
+#include <priority_queue>
+int furthestBuilding( std::vector<int> heights, int bricks, int ladders )
+{
+  std::priority_queue<int> used_bricks;
+  for( int i = 0; i < heights.size() - 1; ++i ) {
+    int height_diff = heights[ i + 1 ] - heights[ i ];
+    if( height_diff <= 0 )
+      continue;
+
+    if( bricks >= height_diff ) {
+      bricks -= height_diff;
+      used_bricks.push( height_diff );
+    } else if( ladders ) {
+      if( !used_bricks.empty() && used_bricks.top() > height_diff ) {
+        int rec_bricks = used_bricks.top();
+        used_bricks.pop();
+        bricks += rec_bricks;
+        bricks -= height_diff;
+        --ladders;
+        used_bricks.push( height_diff );
+      }
+    } else 
+      return i;
+  }
+
+  return heights.size() - 1;
+}
