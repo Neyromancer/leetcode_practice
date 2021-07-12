0ad52165b0151393314b963fbc905c9e68f48beb
Neyroman <neyroman@neyroman.net>
2021-07-12 23:08:04 +0300

    Longest Subarray of 1st after deleting one lement

---
+++ yandex2021/longest_subarray_of_1st_after_deleting_one_element.cpp
@@ -0,0 +1,78 @@
+class Solution {
+public:
+    // O( N ), N == nums.size()
+    // O( 1 )
+    int longestSubarray( vector<int>& nums )
+    {
+        int l{ 0 };
+        int len{ 0 };
+        int index{ 0 };
+        bool is_zero{ false };
+        int r = 0;
+        for( ; r < nums.size(); ++r ) {
+            if( !nums[ r ] ) {
+                if( is_zero ) {
+                    len = std::max( len, r - l - 1 );
+                    l = index + 1;
+                    index = r;
+                } else {
+                    is_zero = true;
+                    index = r;
+                }
+            }
+        }
+        
+        l = l + 1;
+        len = std::max( len, r - l );
+        
+        return len;
+    }
+};
+
+class Solution {
+public:
+    // time O( N ), N == nums.size
+    // space O( 1 )
+    int longestSubarray( vector<int>& nums )
+    {
+        int prev_cnt{ 0 };
+        int cnt{ 0 };
+        int res{ 0 };
+        for( int i = 0; i < nums.size(); ++i ) {
+            if( nums[ i ] ) {
+                ++cnt;
+            } else {
+                res = std::max( res, prev_cnt + cnt );
+                prev_cnt = cnt;
+                cnt = 0;
+            }
+        }
+        
+        return ( cnt == nums.size() ? cnt - 1 : std::max( res, prev_cnt + cnt ) );
+    }
+};
+
+class Solution {
+public:
+    // time O( N ), N == nums.size
+    // space O( 1 )
+    int longestSubarray(vector<int>& nums)
+    {
+        int cnt{ nums[ 0 ] };
+        int prev{ 0 };
+        int res{ 0 };
+        for( int i = 1; i < nums.size(); ++i ) {
+            if( nums[ i ] ) {
+                prev = std::max( ++prev, cnt );
+                ++cnt;
+            } else {
+                prev = cnt;
+                cnt = 0;
+            }
+            
+            res = std::max( res, prev );
+        }
+        
+        return res;
+    }
+};
