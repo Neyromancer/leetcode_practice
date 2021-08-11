9238d23ad7e516f4c0e1483e2c0e778f7ff23ba7
Neyroman <neyroman@neyroman.net>
2021-08-11 09:52:52 +0300

    Inorder Successor in BST

---
+++ trees/inorder_successor_in_BST.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    // time O( N ), N == number of nodes in the tree
+    // space O( N ), where N == number of nodes in the tree
+    TreeNode* inorderSuccessor( TreeNode* root, TreeNode* p )
+    {
+        if( !root || !p )
+            return nullptr;
+        
+        TreeNode *res{ nullptr };
+        inorder( root, p, res );
+        return res;
+    }
+    
+    void inorder( TreeNode *root, TreeNode *p, TreeNode* &res )
+    {
+        if( !root )
+            return;
+        
+        inorder( root->left, p, res );
+        
+        if( !res && root->val > p->val )
+            res = root;
+        
+        inorder( root->right, p, res );
+    }
+};
+
+class Solution {
+public:
+    // time O( N ), where N == number of nodes in the tree
+    // space O( N ),m where N == number of nodes in the tree
+    TreeNode* inorderSuccessor( TreeNode* root, TreeNode* p ) 
+    {
+        if( !root || !p )
+            return nullptr;
+        
+        std::stack<TreeNode *> stk;
+        while( !stk.empty() || root ) {
+            while( root ) {
+                stk.push( root );
+                root = root->left;
+            }
+            
+            auto node = stk.top();
+            stk.pop();
+            if( node && node->val > p->val )
+                return node;
+            
+            root = node->right;
+        }
+        
+        return nullptr;
+    }
+};
