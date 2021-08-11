3cd98fb7f0dd83af8eb2f7b43ddc46356bd1d7cf
Neyroman <neyroman@neyroman.net>
2021-08-11 09:23:04 +0300

    Validate Binary Search Tree update

--- validate_binary_search_tree.cpp
+++ validate_binary_search_tree.cpp
@@ -58,3 +58,98 @@ public:
             validate( root->left, low, root );
     }
 };
+
+class Solution {
+public:
+    // time O( N ), where N == number of nodes in the tree
+    // space O( N ), where N == number of nodes in the tree
+    bool isValidBST( TreeNode* root )
+    {
+        std::stack<TreeNode *> stk;
+        std::stack<TreeNode *> lower_limits;
+        std::stack<TreeNode *> above_limits;
+        
+        auto update = [ &stk, &lower_limits, &above_limits ]( TreeNode *cur, TreeNode *lborder, TreeNode *rborder ) {
+            stk.push( cur );
+            lower_limits.push( lborder );
+            above_limits.push( rborder );
+        };
+        
+        update( root, nullptr, nullptr );
+        while( !stk.empty() ) {
+            auto node = stk.top();
+            stk.pop();
+            auto low = lower_limits.top();
+            lower_limits.pop();
+            auto high = above_limits.top();
+            above_limits.pop();
+            
+            if( !node )
+                continue;
+            
+            if( low && low->val >= node->val )
+                return false;
+            
+            if( high && high->val <= node->val )
+                return false;
+            
+            update( node->right, node, high );
+            update( node->left, low, node );
+        }
+        
+        return true;
+    }
+};
+
+class Solution {
+    TreeNode *prev{ nullptr };
+public:
+    // time O( N ), where N == nodes in the tree
+    // space O( N ), where N == nodes in the tree
+    bool isValidBST(TreeNode* root)
+    {
+        return inorder( root );
+    }
+    
+    bool inorder( TreeNode *root )
+    {
+        if( !root )
+            return true;
+        
+        if( !inorder( root->left ) )
+            return false;
+        
+        if( prev && prev->val >= root->val )
+            return false;
+        
+        prev = root;
+        return inorder( root->right );
+    }
+};
+
+class Solution {
+public:
+    // time O( N ), where N == number of nodes in the tree
+    // space O( N ), where N == number of nodes in the tree
+    bool isValidBST( TreeNode* root )
+    {
+        std::stack<TreeNode *> stk;
+        TreeNode *prev{ nullptr };
+        while( !stk.empty() || root ) {
+            while( root ) {
+                stk.push( root );
+                root = root->left;
+            }
+            
+            auto node = stk.top();
+            stk.pop();
+            if( prev && prev->val >= node ->val )
+                return false;
+            
+            prev = node;
+            root = node->right;
+        }
+        
+        return true;
+    }
+};
