690ff48e1e3262f554fe4909d2e75e8d7664353c
Dmitry Kormulev <dmitrii.kormulev@mapbox.com>
2021-09-17 22:53:13 +0300

    Longest Repeating Character Replacement

--- longest_repeating_character_replacement.cpp
+++ longest_repeating_character_replacement.cpp
@@ -1,26 +1,43 @@
-#include <array>
-#include <string>
-
-
-// space O( 1 )
-// time O( N )
-int characterReplacement( std::string s, int k ) {
-  if( s.empyt() )
-    return 0;
-
-  int start{ 0 };
-  int max_repeating_chars{ 0 };
-  std::array<int, 26> alpha{};
-  int res{ 0 };
-  for( int end = 0; end < s.size(); ++end ) {
-    max_repeating_chars = std::max( max_repeating_chars, ++alpha[ s[ end ] - 'A' ] );
-    while( end - start + 1 - max_repeating_chars > k ) {
-      --alpha[ s[ end ] - 'A' ];
-      ++start;
+class Solution {
+public:
+    // time O( sizeof( s ) )
+    // space O( 1 )
+    std::unordered_set<char>
+    find_all_uniques( std::string_view s )
+    {
+        std::unordered_set<char> st;
+        for( char ch : s )
+            st.insert( ch );
+        return st;
     }
+    
+    // time O( sizeof( s ) )
+    // space O( 1 )
+    int characterReplacement(string s, int k) 
+    {
+        auto uniques = find_all_uniques(s);
+        int len{ 0 };
 
-    res = std::max( res, end - start + 1 );
-  }
-
-  return res;
-}
+        for( char ch : uniques ) {  // time O( 1 )
+            int l{ 0 };
+            int r{ 0 };
+            int cnt = k;
+            while( r < s.size() ) { // O( sizeof( s ))
+                if( ch != s[ r ] )
+                    --cnt;
+                
+                while( cnt < 0 && l < r ) {
+                    if( ch != s[ l++ ] )
+                        ++cnt;
+                }
+                
+                len = std::max( len, r - l + 1 );
+                ++r;
+            }
+            
+            len = std::max( len, r - l );
+        }
+        
+        return len;
+    }
+};
