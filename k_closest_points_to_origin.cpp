c4ec5ec9e639c3acc4a4a9454ea9f01366146396
Dmitry Kormulev <dmitrii.kormulev@mapbox.com>
2021-11-25 10:07:22 +0300

    K closest Points to Origin

--- k_closest_points_to_origin.cpp
+++ k_closest_points_to_origin.cpp
@@ -31,3 +31,71 @@ std::vector<std::vector<int>> KClosest( std::vector<std::vector<int>> &points, i
 
   return res;
 }
+
+class Solution {
+public:
+    double calc_dist( const std::vector<int>& ptr ) {
+        return std::sqrt( std::pow( ptr[ 0 ], 2 ) + std::pow( ptr[ 1 ], 2 ) );
+    }
+
+    // time O( N * k log k )
+    // space O( k )
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k)
+    {
+         auto cmp = []( const std::pair<double, std::vector<int>>& p1,
+                        const std::pair<double, std::vector<int>>& p2 ) {
+             return std::get<0>( p1 ) <= std::get<0>( p2 );
+         };
+        
+        std::priority_queue<std::pair<double, std::vector<int>>, 
+        std::vector<std::pair<double, std::vector<int>>>, decltype(cmp)> q(cmp);
+        for( const auto& ptr : points ) {
+            auto dist = calc_dist( ptr );
+            q.push( std::make_pair( dist, ptr ) ); // O( k log k )
+            if( q.size() > k )
+                q.pop();// O( k log k )
+        }
+        
+        std::vector<std::vector<int>> res;
+        res.reserve( k );
+        while( !q.empty() ) {
+            auto node = q.top(); // O( k log k )
+            res.push_back( std::move( std::get<1>( node ) ) ); // O( k log k )
+            q.pop();
+        }
+        
+        return res;
+    }
+};
+
+class Solution {
+public:
+    double calc_dist( const std::vector<int>& ptr ) {
+        return std::sqrt( std::pow( ptr[ 0 ], 2 ) + std::pow( ptr[ 1 ], 2 ) );
+    }
+
+    // time O( N log N )
+    // space O( N )
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k)
+    {
+         auto cmp = []( const std::pair<double, std::vector<int>>& p1,
+                        const std::pair<double, std::vector<int>>& p2 ) {
+             return std::get<0>( p1 ) <= std::get<0>( p2 );
+         };
+
+        std::vector<std::pair<double, std::vector<int>>> vc;
+        vc.reserve( points.size() );
+        for( const auto& ptr : points )
+            vc.push_back( std::make_pair( calc_dist( ptr ), ptr ) );
+
+        std::sort( std::begin( vc ), std::end( vc ), cmp );
+        
+        
+        std::vector<std::vector<int>> res;
+        res.reserve( k );
+        for( int i = 0; i < k; ++i )
+            res.push_back( std::move( std::get<1>( vc[ i ] ) ) );
+
+        return res;
+    }
+};
